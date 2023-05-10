5d8ceb5777f62334a0ae87a219ca5048cb3e8742
abdala9512 <miguel.arquez12@gmail.com>
2023-05-09 21:36:28 -0500

    dash: Streamlit app -deactivate openai

--- dashboard/dash_function.py
+++ dashboard/dash_function.py
@@ -1,7 +1,6 @@
 
 import openai
 
-openai.api_key = "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"
 
 def get_completion(prompt: str, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
